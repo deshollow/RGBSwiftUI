2634762f3a9bbb027941eb96cd3a1b04894f1b39
deshollow <555demin@gmail.com>
2023-12-16 11:40:15 +0300

    Initial Commit

---
+++ RGBSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RGBSwiftUI
+//
+//  Created by deshollow on 16.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RGBSwiftUI/RGBSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBSwiftUIApp.swift
+//  RGBSwiftUI
+//
+//  Created by deshollow on 16.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct RGBSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
